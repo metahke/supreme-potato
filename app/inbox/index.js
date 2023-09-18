a9bba6aba12fc645fe2883f3dd154414d4178fa9
purplehepatica <fioletowy.temp@tutanota.com>
2023-09-18 20:47:05 +0200

    Updates // inbox, localStorage

--- app/inbox/index.js
+++ app/inbox/index.js
@@ -46,13 +46,90 @@ function loadDataFromLocalStorage() {
 loadDataFromLocalStorage();
 
 
-
 function saveData() {
     console.log(localStorage.getItem("inboxData"));
     console.log(JSON.stringify(appData));
 
     localStorage.setItem("inboxData", JSON.stringify(appData));
 }
+
+
+
+
+
+function addToInboxListener() {
+
+    const addToInboxButton = document.querySelector(".add-to-inbox-button");
+    const addToInboxInputField = document.querySelector(".add-inbox-input-field");
+
+    addToInboxButton.addEventListener("click", () => {
+
+        const inputValue = addToInboxInputField.value;
+
+        if (inputValue !== "") {
+
+            appData.inbox.push(inputValue);
+
+            document.querySelector(".inbox-elements").innerHTML += `<article><p>${inputValue.replace("- ", "")}</p></article>`;
+
+            addToInboxInputField.value = null;
+            saveData();
+        }
+    })
+}
+
+addToInboxListener()
+
+
+
+
+
+function cleanInbox() {
+
+    const cleanInboxButton = document.querySelector(".clean-inbox-button");
+    const cleanInboxContainer = document.querySelector(".clean-inbox-container");
+    const closeCleanInboxDialogButton = document.querySelector(".clean-close");
+    const closeInboxElement = document.querySelector(".close-inbox-element");
+
+
+    cleanInboxButton.addEventListener("click", () => {
+
+        cleanInboxContainer.querySelector("dialog").setAttribute("open", "true");
+
+        const length = appData.inbox.length - 1;
+        const randomItem = appData.inbox[Math.round(Math.random() * length)];
+
+        closeInboxElement.innerHTML = `<article><p>${randomItem}</p></article>`
+    });
+
+    closeCleanInboxDialogButton.addEventListener("click", (e) => {
+        e.target.closest("dialog").removeAttribute("open")
+    })
+
+
+
+}
+
+cleanInbox()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -89,16 +166,16 @@ function editTasksModeOn() {
     let texts = [...paragraphContent].map(article => {
         return `- ${article.textContent}`;
     })
-    console.log(paragraphContent);
-    console.log(texts);
 
-    document.querySelector(".inbox-paragraph").innerHTML = `
+    document.querySelector(".inbox-textarea-container").innerHTML = `
         <textarea style="height: 300px" class="inbox-textarea"></textarea>
     `;
 
     if (paragraphContent.length >= 1) {
         document.querySelector(".inbox-textarea").textContent = texts.join("\n");
     }
+
+    document.querySelector(".inbox-elements").innerHTML = null;
 
     elements.editInboxButton.querySelector("span").textContent = "thumb_up";
 }
@@ -107,7 +184,7 @@ function editTasksModeOff() {
 
     const textAreaContent = document.querySelector(".inbox-textarea").value.split("\n");
 
-    document.querySelector(".inbox-paragraph").innerHTML = null;
+    document.querySelector(".inbox-textarea-container").innerHTML = null;
     document.querySelector(".inbox-elements").innerHTML = null;
 
     appData.inbox = [];
@@ -117,7 +194,7 @@ function editTasksModeOff() {
 
             appData.inbox.push(line);
 
-            document.querySelector(".inbox-paragraph").innerHTML += `<article><p>${line.replace("- ", "")}</p></article>`;
+            document.querySelector(".inbox-elements").innerHTML += `<article><p>${line.replace("- ", "")}</p></article>`;
         }
     }
 
